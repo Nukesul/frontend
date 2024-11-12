99177bb2b45fbddb61ed6530aa536f3ab441519b
It developer 😎 <159410366+Nukesul@users.noreply.github.com>
2024-11-12 18:51:15 +0600

    3

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -50,28 +50,30 @@ const OrderPage = () => {
   };
 
   // Функция для отправки данных заказа в Telegram
-  const sendOrderToTelegram = async () => {
-    try {
-      const response = await fetch('https://nukesul-backend-1bde.twc1.net/api/send-order', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          orderDetails,
-          deliveryDetails,
-          cartItems,
-        }),
-      });
 
-      if (response.ok) {
-        alert('Заказ успешно отправлен в Telegram!');
-      } else {
-        alert('Ошибка при отправке заказа.');
-      }
-    } catch (error) {
-      console.error('Ошибка:', error);
+const sendOrderToTelegram = async () => {
+  try {
+    // Преобразуем объекты данных в строку запроса для URL
+    const queryParams = new URLSearchParams({
+      orderDetails: JSON.stringify(orderDetails),
+      deliveryDetails: JSON.stringify(deliveryDetails),
+      cartItems: JSON.stringify(cartItems),
+    });
+
+    const response = await fetch(`https://nukesul-backend-1bde.twc1.net/api/send-order?${queryParams.toString()}`, {
+      method: 'GET',  // Используем GET-запрос
+    });
+
+    if (response.ok) {
+      alert('Заказ успешно отправлен в Telegram!');
+    } else {
       alert('Ошибка при отправке заказа.');
     }
-  };
+  } catch (error) {
+    console.error('Ошибка:', error);
+    alert('Ошибка при отправке заказа.');
+  }
+};
 
   return (
     <div className="order-page">
